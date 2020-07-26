bd2c03972646f35fbaa0b212287ec75540d79b7e
Armaan Shivpuri <armaan@shivpuri.com>
2020-07-26 15:46:16 -0700

    Create project.py

---
+++ project.py
@@ -0,0 +1,21 @@
+import os
+import playsound
+from gtts import gTTS
+import random
+import string
+from tkinter import *
+
+    letters = string.ascii_letters
+    return ''.join(random.choice(letters) for i in range(stringLength))
+
+def speak(text):
+	tts = gTTS(text=text, lang="en")
+	name = randomString(7)
+	filename = name +".mp3"
+	tts.save(filename)
+	playsound.playsound(filename)
+	os.remove(filename)
+
+tk = Tk()
+btn = Button(tk, text = "click me", command=speak("hell0"))
+btn.pack()
